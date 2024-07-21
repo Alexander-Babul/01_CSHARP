a5816b9d0ffe44cbeb2fd7ee0f8e8a057c601cbd
AlexanderBabul <alexanderbabul@gmail.com>
2024-07-22 00:44:38 +0300

    ZadachaOpredeleniyaBolhegoChislaIzPyati

---
+++ Desktop/01_CSHARP/Lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 9;
+int b = 8;
+int c = 10;
+int d = 6;
+int e = 5;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
