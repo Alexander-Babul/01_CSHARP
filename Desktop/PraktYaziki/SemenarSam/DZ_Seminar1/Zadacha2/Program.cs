803aee6fcda2377255885438c737233060b4f433
AlexanderBabul <alexanderbabul@gmail.com>
2024-07-24 22:32:50 +0300

    ResilalZadaca2_

---
+++ Desktop/PraktYaziki/SemenarSam/DZ_Seminar1/Zadacha2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три
+// числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 => 7
+// 44, 5, 78 => 78
+// 22, 3, 9 => 22
+
+Console.Write("Напишите первое число: ");
+int firsnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Напишите третье число: ");
+int thirdnumber = Convert.ToInt32(Console.ReadLine());
+
+int maximal = firsnumber;
+
+if(secondnumber > maximal)
+{
+    int max = secondnumber;
+}
+
+if(thirdnumber > maximal)
+{
+    int max = secondnumber;
+}
+
+Console.Write($"Максимальное число = {maximal}");
