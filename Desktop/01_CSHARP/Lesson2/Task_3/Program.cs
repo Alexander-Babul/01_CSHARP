82800a8d5815fed5bae5271c0e71fbc2f5f1b040
AlexanderBabul <alexanderbabul@gmail.com>
2024-08-08 20:39:22 +0300

    Dobavil_Передачу_параметров_по_ссылке_массив

---
+++ Desktop/01_CSHARP/Lesson2/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿void ZeroEvenElement(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] Array = { 1, 3, 6, 8, 9, 8, 8 };
+
+ZeroEvenElement(Array);
+PrintArray(Array);
