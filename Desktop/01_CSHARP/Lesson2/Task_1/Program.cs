08ab2f4f25901fa476bcf63f5e3e823eab28ef67
AlexanderBabul <alexanderbabul@gmail.com>
2024-08-08 19:40:12 +0300

    Dobavil_funkcii

---
+++ Desktop/01_CSHARP/Lesson2/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double Nominator = a * b;
+    int Denaminator = c +d;
+    double result = Nominator / Denaminator;
+    return result;
+}
+
+double result = CalculateFormula(1,2,3,4);
+Console.WriteLine(result);
